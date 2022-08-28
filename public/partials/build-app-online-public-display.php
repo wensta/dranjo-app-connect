e4012756ff8d54c52d9a19216d3b22660ab8cac8
Dranjo <92259456+dranjocloud@users.noreply.github.com>
2022-08-29 05:19:32 +0530

    Create build-app-online-public-display.php

---
+++ public/partials/build-app-online-public-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @link       https://buildapp.online
+ * @since      1.0.0
+ *
+ * @package    Build_App_Online
+ * @subpackage Build_App_Online/public/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
