a6a662db678e66979ec9fdec1b8ab5c5b643e990
Dranjo <92259456+dranjocloud@users.noreply.github.com>
2022-08-29 04:35:07 +0530

    Create build-app-online-admin-display.php

---
+++ admin/partials/build-app-online-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://buildapp.online
+ * @since      1.0.0
+ *
+ * @package    Build_App_Online
+ * @subpackage Build_App_Online/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
